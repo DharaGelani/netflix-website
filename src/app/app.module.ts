77c5bde9f85898b1c65d32dc1a45ffeb59218ca1
Dhara <dhara.gelani@gmail.com>
2021-11-17 08:57:57 -0500

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HomeBannerComponent } from './home-banner/home-banner.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { FaqComponent } from './faq/faq.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    HomeBannerComponent,
+    ProductListComponent,
+    FaqComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
